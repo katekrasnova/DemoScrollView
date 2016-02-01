9316f658e5f5165fe6238c2b3ee58fb5c0df0ccf
katekrasnova <katekrasnova1990@gmail.com>
2016-02-01 14:23:27 +0300

    Initial Commit

---
+++ ScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewDemo
+//
+//  Created by Ekaterina Krasnova on 01.02.16.
+//  Copyright © 2016 Ekaterina Krasnova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewDemo
+//
+//  Created by Ekaterina Krasnova on 01.02.16.
+//  Copyright © 2016 Ekaterina Krasnova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
